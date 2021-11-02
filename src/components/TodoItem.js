55a35b0197e163d771a83476ceae3088d416e6b7
ahmedadel56 <ahmdadl563@gmail.com>
2021-11-02 11:14:56 +0200

    Add Editing functionality

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,7 +1,8 @@
-import React from "react"
+import React ,{useState}from "react"
 import styles from "./TodoItem.module.css";
 
 const TodoItem = (props)=> {
+
     const completedStyle = {
         fontStyle: "italic",
         color: "#595959",
@@ -9,23 +10,53 @@ const TodoItem = (props)=> {
         textDecoration: "line-through",
       };
       const { completed, id, title } = props.todo;
+      const [state, setState] = useState({
+        edit:false
+      });
+    
 
-      
+     const handleEditing = (e)=> {
+        setState({...state,edit:true});
+      }
+      let viewMode = {}
+      let editMode = {}
+
+      if (state.edit) {
+      viewMode.display = "none"
+      } else {
+      editMode.display = "none"
+      }
+     const handleUpdatedDone = event => {
+        if (event.key === "Enter") {
+          setState({ edit: false })
+        }
+      }
     return (
         <li className={styles.item}>
-          <input
-            type="checkbox"
-            className={styles.checkbox}
-            checked={completed}
-            onChange={() => props.handleChangeProps(id)}
-          />
-          <button onClick={() => props.deleteTodoProps(id)}>
-            Delete
-          </button>
-          <span style={completed ? completedStyle : null}>
+          <div onDoubleClick = {handleEditing} style={viewMode}>
+            <input
+              type="checkbox"
+              className={styles.checkbox}
+              checked={completed}
+              onChange={() => props.handleChangeProps(id)}
+            />
+            <button onClick={() => props.deleteTodoProps(id)}>
+              Delete
+            </button>
+            <span style={completed ? completedStyle : null}>
 
-          {title}
-          </span>
+            {title}
+            </span>
+          </div>
+
+          <input type="text"
+           style={editMode}
+           className="textInput"
+           value={title}
+           onChange={e => {
+            props.setUpdate(e.target.value, id)
+          }}
+          onKeyDown={handleUpdatedDone} />
         </li>
       )
   
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -6,7 +6,8 @@ const TodosList = (props) => {
         {props.todos.map((todo) => (
             <TodoItem key={todo.id} todo={todo}
              handleChangeProps={props.handleChangeProps} 
-             deleteTodoProps={props.deleteTodoProps}/>
+             deleteTodoProps={props.deleteTodoProps}
+             setUpdate={props.setUpdate}/>
         ))}
       </ul>
     );
